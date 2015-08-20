1bad159b1e5d714672c44fd7a3d53abb8ea2cac3
RuriKameoka <tortoise.r.k@gmail.com>
2015-08-20 22:13:46 +0900

    unityBall
    ・一方通行ブロックの追加
    ・ブーストした時の壁との衝突判定

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public int boostCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			boostCount += 1;
+			//print(boostCount);
+		} 
+
+	}
+
+	void OnCollisionEnter(Collision hit){
+		if(hit.gameObject.tag == "wall"){
+            if(boostCount > 2){
+            	print("boom");
+            }
+            //print("hit");
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other){
+    	print("OK");
+    	if(other.gameObject.tag == "cubeTrigger"){
+    		other.GetComponent<Collider>().isTrigger = false;
+    	}
+    	
+    }
+
+}
